5be96e3e0cba6f9d3b93d7201bf47379e6055908
whw-baird <wbaird@chicagobooth.edu>
2022-05-27 20:48:04 +0000

    Temporary fix for cocktail comment form; added bar feed

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -2,5 +2,5 @@ class Bar < ApplicationRecord
   belongs_to :owner, class_name: "User"
 
   has_many :barflies
-  has_many :cocktails
+  has_many :own_cocktails, class_name: "Cocktail", dependent: :destroy
 end
